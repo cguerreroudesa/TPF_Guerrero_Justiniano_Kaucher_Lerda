fe1d585ca38d218cbd4efd3461ee962d2f3961f6
Camila Guerrero <cguerrero@udesa.edu.ar>
2022-07-06 15:59:25 -0300

    informe

---
+++ test_instrument.py
@@ -0,0 +1,10 @@
+import unittest
+import instrumento
+
+class ModulatorFunctions:
+    def __init__(self,filename):
+         instrument = instrument.class_instrument('piano',)
+         self.assertEqual(instrument.filename, 'piano')
+        
+if __name__ == '__main__':
+    unittest.main()
